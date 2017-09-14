e082f810ccf1dcbff319c8f20479dd5965d2a330
xorz57 <fot@ceid.upatras.gr>
2017-09-14 22:49:07 +0300

    Nested namespace for each tree data structure

--- include/forest/avl_tree.h
+++ include/forest/avl_tree.h
@@ -2,9 +2,8 @@
 #define AVL_TREE_H
 
 namespace forest {
-        template <typename key_t, typename value_t>
-        class avl_tree {
-        private:
+        namespace avl {
+                template <typename key_t, typename value_t>
                 struct node {
                         node(key_t key, value_t value) {
                                 this->key = key;
@@ -18,44 +17,49 @@ namespace forest {
                         node *parent;
                         node *left;
                         node *right;
-                } *root;
-                unsigned long long nodes;
-                void pre_order_traversal(node *n) {
-                        if (n == nullptr) return;
-                        // process the node n
-                        pre_order_traversal(n->left);
-                        pre_order_traversal(n->right);
-                }
-                void in_order_traversal(node *n) {
-                        if (n == nullptr) return;
-                        in_order_traversal(n->left);
-                        // process the node n
-                        in_order_traversal(n->right);
-                }
-                void post_order_traversal(node *n) {
-                        if (n == nullptr) return;
-                        post_order_traversal(n->left);
-                        post_order_traversal(n->right);
-                        // process the node n
-                }
-        public:
-                avl_tree() {
-                        root = nullptr;
-                        nodes = 0;
-                }
-                ~avl_tree() {
+                };
+                template <typename key_t, typename value_t>
+                class tree {
+                private:
+                        node <key_t, value_t> *root;
+                        unsigned long long nodes;
+                        void pre_order_traversal(node <key_t, value_t> *n) {
+                                if (n == nullptr) return;
+                                // process the node n
+                                pre_order_traversal(n->left);
+                                pre_order_traversal(n->right);
+                        }
+                        void in_order_traversal(node <key_t, value_t> *n) {
+                                if (n == nullptr) return;
+                                in_order_traversal(n->left);
+                                // process the node n
+                                in_order_traversal(n->right);
+                        }
+                        void post_order_traversal(node <key_t, value_t> *n) {
+                                if (n == nullptr) return;
+                                post_order_traversal(n->left);
+                                post_order_traversal(n->right);
+                                // process the node n
+                        }
+                public:
+                        tree() {
+                                root = nullptr;
+                                nodes = 0;
+                        }
+                        ~tree() {
 
-                }
-                void pre_order_traversal() {
-                        pre_order_traversal(root);
-                }
-                void in_order_traversal() {
-                        in_order_traversal(root);
-                }
-                void post_order_traversal() {
-                        post_order_traversal(root);
-                }
-        };
+                        }
+                        void pre_order_traversal() {
+                                pre_order_traversal(root);
+                        }
+                        void in_order_traversal() {
+                                in_order_traversal(root);
+                        }
+                        void post_order_traversal() {
+                                post_order_traversal(root);
+                        }
+                };
+        }
 }
 
 #endif
--- include/forest/red_black_tree.h
+++ include/forest/red_black_tree.h
@@ -2,9 +2,8 @@
 #define RED_BLACK_TREE_H
 
 namespace forest {
-        template <typename key_t, typename value_t>
-        class red_black_tree {
-        private:
+        namespace red_black {
+                template <typename key_t, typename value_t>
                 struct node {
                         node(key_t key, value_t value) {
                                 this->key = key;
@@ -18,44 +17,49 @@ namespace forest {
                         node *parent;
                         node *left;
                         node *right;
-                } *root;
-                unsigned long long nodes;
-                void pre_order_traversal(node *n) {
-                        if (n == nullptr) return;
-                        // process the node n
-                        pre_order_traversal(n->left);
-                        pre_order_traversal(n->right);
-                }
-                void in_order_traversal(node *n) {
-                        if (n == nullptr) return;
-                        in_order_traversal(n->left);
-                        // process the node n
-                        in_order_traversal(n->right);
-                }
-                void post_order_traversal(node *n) {
-                        if (n == nullptr) return;
-                        post_order_traversal(n->left);
-                        post_order_traversal(n->right);
-                        // process the node n
-                }
-        public:
-                red_black_tree() {
-                        root = nullptr;
-                        nodes = 0;
-                }
-                ~red_black_tree() {
+                };
+                template <typename key_t, typename value_t>
+                class tree {
+                private:
+                        node <key_t, value_t> *root;
+                        unsigned long long nodes;
+                        void pre_order_traversal(node <key_t, value_t> *n) {
+                                if (n == nullptr) return;
+                                // process the node n
+                                pre_order_traversal(n->left);
+                                pre_order_traversal(n->right);
+                        }
+                        void in_order_traversal(node <key_t, value_t> *n) {
+                                if (n == nullptr) return;
+                                in_order_traversal(n->left);
+                                // process the node n
+                                in_order_traversal(n->right);
+                        }
+                        void post_order_traversal(node <key_t, value_t> *n) {
+                                if (n == nullptr) return;
+                                post_order_traversal(n->left);
+                                post_order_traversal(n->right);
+                                // process the node n
+                        }
+                public:
+                        tree() {
+                                root = nullptr;
+                                nodes = 0;
+                        }
+                        ~tree() {
 
-                }
-                void pre_order_traversal() {
-                        pre_order_traversal(root);
-                }
-                void in_order_traversal() {
-                        in_order_traversal(root);
-                }
-                void post_order_traversal() {
-                        post_order_traversal(root);
-                }
-        };
+                        }
+                        void pre_order_traversal() {
+                                pre_order_traversal(root);
+                        }
+                        void in_order_traversal() {
+                                in_order_traversal(root);
+                        }
+                        void post_order_traversal() {
+                                post_order_traversal(root);
+                        }
+                };
+        }
 }
 
 #endif
