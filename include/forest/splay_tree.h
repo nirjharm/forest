5da99472226475f09c8acf6c116f2cfa017a6057
George Fotopoulos <xorz57@gmail.com>
2017-09-16 18:59:05 +0300

    Core operations of the splay tree are done

--- include/forest/splay_tree.h
+++ include/forest/splay_tree.h
@@ -7,6 +7,11 @@ namespace forest {
         namespace splay {
                 template <typename key_t, typename value_t>
                 struct node {
+                        key_t key;
+                        value_t value;
+                        node *parent;
+                        node *left;
+                        node *right;
                         node(key_t key, value_t value) {
                                 this->key = key;
                                 this->value = value;
@@ -14,64 +19,46 @@ namespace forest {
                                 this->left = nullptr;
                                 this->right = nullptr;
                         }
-                        key_t key;
-                        value_t value;
-                        node *parent;
-                        node *left;
-                        node *right;
+                        void info() {
+                                std::cout << this->key << "\t";
+                                if (this->left != nullptr) {
+                                        std::cout << this->left->key << "\t";
+                                } else {
+                                        std::cout << "null" << "\t";
+                                }
+                                if (this->right != nullptr) {
+                                        std::cout << this->right->key << "\t";
+                                } else {
+                                        std::cout << "null" << "\t";
+                                }
+                                if (this->parent != nullptr) {
+                                        std::cout << this->parent->key << std::endl;
+                                } else {
+                                        std::cout << "null" << std::endl;
+                                }
+                        }
                 };
                 template <typename key_t, typename value_t>
                 class tree {
                 private:
                         node <key_t, value_t> *root;
-                        void pre_order_traversal(node <key_t, value_t> *n) {
-                                if (n == nullptr) return;
-                                // std::cout << n->key << " " << n->value << std::endl;
-                                pre_order_traversal(n->left);
-                                pre_order_traversal(n->right);
+                        void pre_order_traversal(node <key_t, value_t> *x) {
+                                if (x == nullptr) return;
+                                x->info();
+                                pre_order_traversal(x->left);
+                                pre_order_traversal(x->right);
                         }
-                        void in_order_traversal(node <key_t, value_t> *n) {
-                                if (n == nullptr) return;
-                                in_order_traversal(n->left);
-                                std::cout << n->key << "\t";
-                                if (n->left != nullptr) {
-                                        std::cout << n->left->key << "\t";
-                                } else {
-                                        std::cout << "null" << "\t";
-                                }
-                                if (n->right != nullptr) {
-                                        std::cout << n->right->key << "\t";
-                                } else {
-                                        std::cout << "null" << "\t";
-                                }
-                                if (n->parent != nullptr) {
-                                        std::cout << n->parent->key << std::endl;
-                                } else {
-                                        std::cout << "null" << std::endl;
-                                }
-                                in_order_traversal(n->right);
+                        void in_order_traversal(node <key_t, value_t> *x) {
+                                if (x == nullptr) return;
+                                in_order_traversal(x->left);
+                                x->info();
+                                in_order_traversal(x->right);
                         }
-                        void post_order_traversal(node <key_t, value_t> *n) {
-                                if (n == nullptr) return;
-                                post_order_traversal(n->left);
-                                post_order_traversal(n->right);
-                                // std::cout << n->key << " " << n->value << std::endl;
-                        }
-                public:
-                        tree() {
-                                root = nullptr;
-                        }
-                        ~tree() {
-
-                        }
-                        void pre_order_traversal() {
-                                pre_order_traversal(root);
-                        }
-                        void in_order_traversal() {
-                                in_order_traversal(root);
-                        }
-                        void post_order_traversal() {
-                                post_order_traversal(root);
+                        void post_order_traversal(node <key_t, value_t> *x) {
+                                if (x == nullptr) return;
+                                post_order_traversal(x->left);
+                                post_order_traversal(x->right);
+                                x->info();
                         }
                         void left_rotate(node <key_t, value_t> *x) {
                                 node <key_t, value_t> *y = x->right;
@@ -135,6 +122,22 @@ namespace forest {
                                         }
                                 }
                         }
+                public:
+                        tree() {
+                                root = nullptr;
+                        }
+                        ~tree() {
+
+                        }
+                        void pre_order_traversal() {
+                                pre_order_traversal(root);
+                        }
+                        void in_order_traversal() {
+                                in_order_traversal(root);
+                        }
+                        void post_order_traversal() {
+                                post_order_traversal(root);
+                        }
                         void insert(key_t key, value_t value) {
                                 node <key_t, value_t> *current = root;
                                 node <key_t, value_t> *previous = nullptr;
@@ -161,14 +164,14 @@ namespace forest {
                                 return (search(key) == nullptr) ? true : false;
                         }
                         node <key_t, value_t> *search(key_t key) {
-                                node <key_t, value_t> *n = root;
-                                while (n != nullptr) {
-                                        if (key > n->key) {
-                                                n = n->right;
-                                        } else if (key < n->key) {
-                                                n = n->left;
+                                node <key_t, value_t> *x = root;
+                                while (x != nullptr) {
+                                        if (key > x->key) {
+                                                x = x->right;
+                                        } else if (key < x->key) {
+                                                x = x->left;
                                         } else {
-                                                return n;
+                                                return x;
                                         }
                                 }
                                 return nullptr;
