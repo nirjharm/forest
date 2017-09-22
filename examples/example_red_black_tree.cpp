dcc7a799ea8e061998d247df52af51637053a9d1
xorz57 <fot@ceid.upatras.gr>
2017-09-22 23:20:02 +0300

    Better Example Files
    Min and Max may not exist in general.

--- examples/example_red_black_tree.cpp
+++ examples/example_red_black_tree.cpp
@@ -33,10 +33,14 @@ int main(int argc, char const *argv[]) {
         std::cout << std::endl;
 
         auto min = red_black_tree.minimum();
-        std::cout << "Minimum: " << min->key << std::endl;
+        if (min != nullptr) {
+                std::cout << "Minimum: " << min->key << std::endl;
+        }
 
         auto max = red_black_tree.maximum();
-        std::cout << "Maximum: " << max->key << std::endl;
+        if (max != nullptr) {
+                std::cout << "Maximum: " << max->key << std::endl;
+        }
 
         std::cout << "Height: " << red_black_tree.height() << std::endl;
 
