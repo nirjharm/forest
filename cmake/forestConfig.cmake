4a90ff8a7d63f7330e084e41d90d9b3b57d74994
Oleksii Vilchanskyi <oleksii.vilchanskyi@gmail.com>
2017-10-20 21:32:41 +0200

    [CMake] Remove copyright notice
    References a294243. That commit missed one notice, this one fixes it.

--- cmake/forestConfig.cmake
+++ cmake/forestConfig.cmake
@@ -1,7 +1,1 @@
-# Copyright (C) 2017 Oleksii Vilchanskyi <alexvilchansky@yahoo.com>
-#
-# This software may be modified and distributed under the terms
-# of the MIT license.  See the LICENSE file for details.
-#
-
 include("${CMAKE_CURRENT_LIST_DIR}/forestTargets.cmake")
