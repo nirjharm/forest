ca96b4d7cce70286521d28868c7c66546bb86fb7
George Fotopoulos <xorz57@gmail.com>
2018-01-04 18:59:50 +0200

    Better bash scripts

---
+++ build_tests.sh
@@ -0,0 +1,3 @@
+cd tests
+curl -O https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch.hpp
+make $MAKEFLAGS
---
+++ run_examples.sh
@@ -0,0 +1,5 @@
+cd examples
+./example_binary_search_tree
+./example_avl_tree
+./example_splay_tree
+./example_red_black_tree
