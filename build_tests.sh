13e23dd4170275209b1e570b6cf235636dc95c1c
George Fotopoulos <xorz57@gmail.com>
2018-01-26 02:35:25 +0200

    Try using <> instead of ""

--- build_tests.sh
+++ build_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd tests
-curl -o include/catch.hpp -O https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch.hpp
+curl -O https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch.hpp -o include/catch.hpp
 make $MAKEFLAGS
