f6be76c0150c84a66fe00627b9275e04df575f26
George Fotopoulos <xorz57@gmail.com>
2018-01-26 03:14:17 +0200

    Update build_tests.sh

--- build_tests.sh
+++ build_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -O https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch.hpp -o include/catch.hpp
+curl -o include/catch.hpp -O https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch.hpp
 cd tests
 make $MAKEFLAGS
