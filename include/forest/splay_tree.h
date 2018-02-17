eb1e07a9b12aba3eedaef3974b577c6fb29e07c0
George Fotopoulos <xorz57@gmail.com>
2018-02-17 18:56:44 -0500

    Removed the value field

--- include/forest/splay_tree.h
+++ include/forest/splay_tree.h
@@ -18,19 +18,17 @@ namespace forest {
         /**
          * @brief Splay Tree node struct
          */
-        template <typename key_t, typename value_t>
+        template <typename key_t>
         struct splay_tree_node {
                 key_t key;     ///< The key of the node
-                value_t value; ///< The value of the node
                 std::weak_ptr<splay_tree_node> parent;    ///< The parent of the node
                 std::shared_ptr<splay_tree_node> left;    ///< The left child of the node
                 std::shared_ptr<splay_tree_node> right;   ///< The right child of the node
                 /**
                  * @brief Constructor of a splay tree node
                  */
-                splay_tree_node(key_t key, value_t value) {
+                splay_tree_node(key_t key) {
                         this->key = key;
-                        this->value = value;
                         this->parent.reset();
                         this->left = nullptr;
                         this->right = nullptr;
@@ -60,49 +58,49 @@ namespace forest {
         /**
          * @brief Splay Tree class
          */
-        template <typename key_t, typename value_t>
+        template <typename key_t>
         class splay_tree {
         private:
-                std::shared_ptr<splay_tree_node <key_t, value_t> > root;
-                void pre_order_traversal(std::shared_ptr<splay_tree_node <key_t, value_t> > &x) {
+                std::shared_ptr<splay_tree_node <key_t> > root;
+                void pre_order_traversal(std::shared_ptr<splay_tree_node <key_t> > &x) {
                         if (x == nullptr) return;
                         x->info();
                         pre_order_traversal(x->left);
                         pre_order_traversal(x->right);
                 }
-                void in_order_traversal(std::shared_ptr<splay_tree_node <key_t, value_t> > &x) {
+                void in_order_traversal(std::shared_ptr<splay_tree_node <key_t> > &x) {
                         if (x == nullptr) return;
                         in_order_traversal(x->left);
                         x->info();
                         in_order_traversal(x->right);
                 }
-                void post_order_traversal(std::shared_ptr<splay_tree_node <key_t, value_t> > &x) {
+                void post_order_traversal(std::shared_ptr<splay_tree_node <key_t> > &x) {
                         if (x == nullptr) return;
                         post_order_traversal(x->left);
                         post_order_traversal(x->right);
                         x->info();
                 }
-                void breadth_first_traversal(std::shared_ptr<splay_tree_node <key_t, value_t> > &x) {
-                        std::queue <std::shared_ptr<splay_tree_node <key_t, value_t> > > queue;
+                void breadth_first_traversal(std::shared_ptr<splay_tree_node <key_t> > &x) {
+                        std::queue <std::shared_ptr<splay_tree_node <key_t> > > queue;
                         if (x == nullptr) return;
                         queue.push(x);
                         while(queue.empty() == false) {
-                                std::shared_ptr<splay_tree_node <key_t, value_t> > y = queue.front();
+                                std::shared_ptr<splay_tree_node <key_t> > y = queue.front();
                                 y->info();
                                 queue.pop();
                                 if (y->left != nullptr) queue.push(y->left);
                                 if (y->right != nullptr) queue.push(y->right);
                         }
                 }
-                unsigned long long height(std::shared_ptr<splay_tree_node <key_t, value_t> > &x) {
+                unsigned long long height(std::shared_ptr<splay_tree_node <key_t> > &x) {
                         if (x == nullptr) return 0;
                         return std::max(height(x->left), height(x->right)) + 1;
                 }
-                unsigned long long size(std::shared_ptr<splay_tree_node <key_t, value_t> > &x) {
+                unsigned long long size(std::shared_ptr<splay_tree_node <key_t> > &x) {
                         if (x == nullptr) return 0;
                         return size(x->left) + size(x->right) + 1;
                 }
-                void graphviz(std::ofstream &file, std::shared_ptr<splay_tree_node <key_t, value_t> > &x, unsigned long long *count) {
+                void graphviz(std::ofstream &file, std::shared_ptr<splay_tree_node <key_t> > &x, unsigned long long *count) {
                         if (x == nullptr) return;
                         graphviz(file, x->left, count);
                         if (x->left != nullptr) {
@@ -121,8 +119,8 @@ namespace forest {
                         }
                         graphviz(file, x->right, count);
                 }
-                void left_rotate(const std::shared_ptr<splay_tree_node <key_t, value_t> > &x) {
-                        std::shared_ptr<splay_tree_node <key_t, value_t> > y = x->right;
+                void left_rotate(const std::shared_ptr<splay_tree_node <key_t> > &x) {
+                        std::shared_ptr<splay_tree_node <key_t> > y = x->right;
                         if(y != nullptr) {
                                 x->right = y->left;
                                 if(y->left != nullptr) y->left->parent = x;
@@ -140,8 +138,8 @@ namespace forest {
                         }
                         x->parent = y;
                 }
-                void right_rotate(const std::shared_ptr<splay_tree_node <key_t, value_t> > &x) {
-                        std::shared_ptr<splay_tree_node <key_t, value_t> > y = x->left;
+                void right_rotate(const std::shared_ptr<splay_tree_node <key_t> > &x) {
+                        std::shared_ptr<splay_tree_node <key_t> > y = x->left;
                         if (y != nullptr) {
                                 x->left = y->right;
                                 if (y->right != nullptr) y->right->parent = x;
@@ -159,16 +157,16 @@ namespace forest {
                         }
                         x->parent = y;
                 }
-                std::shared_ptr<splay_tree_node <key_t, value_t> > find_parent(std::shared_ptr<splay_tree_node <key_t, value_t> > &x) {
+                std::shared_ptr<splay_tree_node <key_t> > find_parent(std::shared_ptr<splay_tree_node <key_t> > &x) {
                         return x->parent.lock();
                 }
-                std::shared_ptr<splay_tree_node <key_t, value_t> > find_grand_parent(std::shared_ptr<splay_tree_node <key_t, value_t> > &x) {
+                std::shared_ptr<splay_tree_node <key_t> > find_grand_parent(std::shared_ptr<splay_tree_node <key_t> > &x) {
                         if (find_parent(x) != nullptr) {
                                 return find_parent(x)->parent.lock();
                         }
                         return nullptr;
                 }
-                void splay(std::shared_ptr<splay_tree_node <key_t, value_t> > &x) {
+                void splay(std::shared_ptr<splay_tree_node <key_t> > &x) {
                         while (find_parent(x) != nullptr) {
                                 if (find_grand_parent(x) == nullptr) {
                                         if (find_parent(x)->left == x) {
@@ -243,12 +241,11 @@ namespace forest {
                 /**
                  * @brief Inserts a new node into the splay tree
                  * @param key The key for the new node
-                 * @param value The value for the new node
                  * @return The the inserted node otherwise nullptr
                  */
-                const std::shared_ptr<splay_tree_node <key_t, value_t> > insert(key_t key, value_t value) {
-                        std::shared_ptr<splay_tree_node <key_t, value_t> > current = root;
-                        std::shared_ptr<splay_tree_node <key_t, value_t> > parent = nullptr;
+                const std::shared_ptr<splay_tree_node <key_t> > insert(key_t key) {
+                        std::shared_ptr<splay_tree_node <key_t> > current = root;
+                        std::shared_ptr<splay_tree_node <key_t> > parent = nullptr;
                         while(current!=nullptr) {
                                 parent = current;
                                 if (key > current->key) {
@@ -259,7 +256,7 @@ namespace forest {
                                         return current;
                                 }
                         }
-                        current = std::make_shared<splay_tree_node <key_t, value_t> >(key, value);
+                        current = std::make_shared<splay_tree_node <key_t> >(key);
                         current->parent = parent;
                         if(parent == nullptr) {
                                 root = current;
@@ -275,8 +272,8 @@ namespace forest {
                  * @brief Performs a binary search starting from the root node
                  * @return The node with the key specified otherwise nullptr
                  */
-                const std::shared_ptr<splay_tree_node <key_t, value_t> > search(key_t key) {
-                        std::shared_ptr<splay_tree_node <key_t, value_t> > x = root;
+                const std::shared_ptr<splay_tree_node <key_t> > search(key_t key) {
+                        std::shared_ptr<splay_tree_node <key_t> > x = root;
                         while (x != nullptr) {
                                 if (key > x->key) {
                                         x = x->right;
@@ -292,8 +289,8 @@ namespace forest {
                  * @brief Finds the node with the minimum key
                  * @return The node with the minimum key otherwise nullptr
                  */
-                const std::shared_ptr<splay_tree_node <key_t, value_t> > minimum() {
-                        std::shared_ptr<splay_tree_node <key_t, value_t> > x = root;
+                const std::shared_ptr<splay_tree_node <key_t> > minimum() {
+                        std::shared_ptr<splay_tree_node <key_t> > x = root;
                         if (x == nullptr) return nullptr;
                         while(x->left != nullptr) x = x->left;
                         return x;
@@ -302,8 +299,8 @@ namespace forest {
                  * @brief Finds the node with the maximum key
                  * @return The node with the maximum key otherwise nullptr
                  */
-                const std::shared_ptr<splay_tree_node <key_t, value_t> > maximum() {
-                        std::shared_ptr<splay_tree_node <key_t, value_t> > x = root;
+                const std::shared_ptr<splay_tree_node <key_t> > maximum() {
+                        std::shared_ptr<splay_tree_node <key_t> > x = root;
                         if (x == nullptr) return nullptr;
                         while(x->right != nullptr) x = x->right;
                         return x;
@@ -312,8 +309,8 @@ namespace forest {
                  * @brief Finds the successor of the node with key specified
                  * @return The successor of the node with key specified otherwise nullptr
                  */
-                const std::shared_ptr<splay_tree_node <key_t, value_t> > successor(key_t key) {
-                        std::shared_ptr<splay_tree_node <key_t, value_t> > x = root;
+                const std::shared_ptr<splay_tree_node <key_t> > successor(key_t key) {
+                        std::shared_ptr<splay_tree_node <key_t> > x = root;
                         while (x != nullptr) {
                                 if (key > x->key) {
                                         x = x->right;
@@ -325,7 +322,7 @@ namespace forest {
                                                 while(x->left != nullptr) x = x->left;
                                                 return x;
                                         }
-                                        std::shared_ptr<splay_tree_node <key_t, value_t> > parent = x->parent.lock();
+                                        std::shared_ptr<splay_tree_node <key_t> > parent = x->parent.lock();
                                         while (parent != nullptr && x == parent->right) {
                                                 x = parent;
                                                 parent = parent->parent.lock();
@@ -339,8 +336,8 @@ namespace forest {
                  * @brief Finds the predecessor of the node with key specified
                  * @return The predecessor of the node with key specified otherwise nullptr
                  */
-                const std::shared_ptr<splay_tree_node <key_t, value_t> > predecessor(key_t key) {
-                        std::shared_ptr<splay_tree_node <key_t, value_t> > x = root;
+                const std::shared_ptr<splay_tree_node <key_t> > predecessor(key_t key) {
+                        std::shared_ptr<splay_tree_node <key_t> > x = root;
                         while (x != nullptr) {
                                 if (key > x->key) {
                                         x = x->right;
@@ -352,7 +349,7 @@ namespace forest {
                                                 while(x->right != nullptr) x = x->right;
                                                 return x;
                                         }
-                                        std::shared_ptr<splay_tree_node <key_t, value_t> > parent = x->parent.lock();
+                                        std::shared_ptr<splay_tree_node <key_t> > parent = x->parent.lock();
                                         while (parent != nullptr && x == parent->left) {
                                                 x = parent;
                                                 parent = parent->parent.lock();
