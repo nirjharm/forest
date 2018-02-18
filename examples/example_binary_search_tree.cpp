0cef73c45ce039cf76ae31e1e989f34933a124a5
xorz57 <fot@ceid.upatras.gr>
2018-02-17 19:09:41 -0500

    Update the examples
    The value field is not available yet

--- examples/example_binary_search_tree.cpp
+++ examples/example_binary_search_tree.cpp
@@ -2,15 +2,15 @@
 #include <iostream>
 
 int main(int argc, char const *argv[]) {
-        forest::binary_search_tree <int, int> binary_search_tree;
+        forest::binary_search_tree <int> binary_search_tree;
 
-        binary_search_tree.insert(4,0);
-        binary_search_tree.insert(2,0);
-        binary_search_tree.insert(90,0);
-        binary_search_tree.insert(3,100);
-        binary_search_tree.insert(0,0);
-        binary_search_tree.insert(14,0);
-        binary_search_tree.insert(45,0);
+        binary_search_tree.insert(4);
+        binary_search_tree.insert(2);
+        binary_search_tree.insert(90);
+        binary_search_tree.insert(3);
+        binary_search_tree.insert(0);
+        binary_search_tree.insert(14);
+        binary_search_tree.insert(45);
 
         std::cout << "Pre Order Traversal" << std::endl;
         std::cout << std::endl;
