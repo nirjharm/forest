e23640cb4a88c1c2b363ad618f3bd566cb3f1332
xorz57 <xorz57@gmail.com>
2018-02-25 23:34:22 +0200

    Rebuild examples and tests

--- build_examples.sh
+++ build_examples.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd examples
+make clean
 make $MAKEFLAGS
--- build_tests.sh
+++ build_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 curl -o include/catch.hpp -O https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch.hpp
 cd tests
+make clean
 make $MAKEFLAGS
