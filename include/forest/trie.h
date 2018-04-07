5bce5b24784eb33d389090b3c805535275377cb6
George Fotopoulos <xorz57@gmail.com>
2018-04-07 03:28:38 +0300

    introduce char_to_int function with exception handling

--- include/forest/trie.h
+++ include/forest/trie.h
@@ -7,8 +7,13 @@
 namespace forest {
 	class trie {
 	private:
+    static constexpr size_t alphabet_size = 1 + 'z' - 'a';
+		int char_to_int(char c) {
+      int index = c - 'a';
+      if (index <0 || index >= alphabet_size) throw std::invalid_argument("oops");
+    }
 		struct Node {
-			std::shared_ptr<Node> children[26];
+			std::shared_ptr<Node> children[alphabet_size];
 			bool end = false;
 		};
 		std::shared_ptr<Node> root = std::make_shared<Node>();
@@ -16,7 +21,7 @@ namespace forest {
 		void insert(const std::string & key) {
 			Node * n = root.get();
 			for (auto c : key) {
-				int index = c - 'a';
+				int index = char_to_int(c);
 				auto& slot = n->children[index];
 				if (!slot) slot = std::make_shared<Node>();
 				n = slot.get();
@@ -26,7 +31,7 @@ namespace forest {
 		bool search(const std::string & key) {
 			Node * n = root.get();
 			for (auto c : key) {
-				int index = c - 'a';
+				int index = char_to_int(c);
 				auto& slot = n->children[index];
 				if (!slot) return false;
 				n = slot.get();
