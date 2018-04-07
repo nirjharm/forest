dc12ba9aafd84292c4d9d050890d0f2d900c5ae3
George Fotopoulos <xorz57@gmail.com>
2018-04-07 11:39:49 +0300

    First attempt to catch an exception

--- include/forest/trie.h
+++ include/forest/trie.h
@@ -5,39 +5,54 @@
 #include <string>
 
 namespace forest {
-  class trie {
-  private:
-    static constexpr size_t alphabet_size = 1 + 'z' - 'a';
-    int char_to_int(char c) {
-      int index = c - 'a';
-      if (index < 0 || index >= alphabet_size) throw std::invalid_argument("oops");
-      return index;
-    }
-    struct Node {
-      std::shared_ptr<Node> children[alphabet_size];
-      bool end = false;
-    };
-    std::shared_ptr<Node> root = std::make_shared<Node>();
-  public:
-    void insert(const std::string & key) {
-      Node * n = root.get();
-      for (auto c : key) {
-        int index = char_to_int(c);
-        auto& slot = n->children[index];
-        if (!slot) slot = std::make_shared<Node>();
-        n = slot.get();
-      }
-      n->end = true;
-    }
-    bool search(const std::string & key) {
-      Node * n = root.get();
-      for (auto c : key) {
-        int index = char_to_int(c);
-        auto& slot = n->children[index];
-        if (!slot) return false;
-        n = slot.get();
-      }
-      return n->end;
-    }
-  };
+	class trie {
+	private:
+		static constexpr size_t alphabet_size = 1 + 'z' - 'a';
+		int char_to_int(char c) {
+			return c - 'a';
+		}
+		bool validate(std::string key) {
+			int index;
+			for (auto c : key) {
+				try {
+					index = char_to_int(c);
+					if (index < 0 || index >= alphabet_size) {
+						throw std::invalid_argument("[forest::trie] unsupported character found");
+					}
+				} catch (std::invalid_argument e) {
+					return false;
+				}
+			}
+			return true;
+		}
+		struct Node {
+			std::shared_ptr<Node> children[alphabet_size];
+			bool end = false;
+		};
+		std::shared_ptr<Node> root = std::make_shared<Node>();
+	public:
+		bool insert(const std::string & key) {
+			if (!validate(key)) return false;
+			Node * n = root.get();
+			for (auto c : key) {
+				int index = char_to_int(c);
+				auto& slot = n->children[index];
+				if (!slot) slot = std::make_shared<Node>();
+				n = slot.get();
+			}
+			n->end = true;
+			return true;
+		}
+		bool search(const std::string & key) {
+			if (!validate(key)) return false;
+			Node * n = root.get();
+			for (auto c : key) {
+				int index = char_to_int(c);
+				auto& slot = n->children[index];
+				if (!slot) return false;
+				n = slot.get();
+			}
+			return n->end;
+		}
+	};
 }
